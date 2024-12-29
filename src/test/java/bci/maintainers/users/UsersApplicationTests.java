8c3b1be7e00c74513503a6f38e7aba60d430df65
Alex Painen <alexpainen@gmail.com>
2024-12-28 22:03:50 -0300

    ADD: Spring Initializr void proyect

---
+++ src/test/java/bci/maintainers/users/UsersApplicationTests.java
@@ -0,0 +1,13 @@
+package bci.maintainers.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
